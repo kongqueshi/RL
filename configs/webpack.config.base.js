a9cde137f61a9620ca6935ec60b9cd63397a18fe
孔雀石 <kongqueshi@wacai.com>
2019-04-24 19:25:35 +0800

    update sqlite3

--- configs/webpack.config.base.js
+++ configs/webpack.config.base.js
@@ -22,13 +22,11 @@ export default {
         }
       },
       // {
-      //   test: /\.less$/,
+      //   test: /\.css$/,
       //   use: [{
       //     loader: "style-loader" // creates style nodes from JS strings
       //   }, {
       //       loader: "css-loader" // translates CSS into CommonJS
-      //   }, {
-      //       loader: "less-loader" // compiles Less to CSS
       //   }]
       // }
     ]
